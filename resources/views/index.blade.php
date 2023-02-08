f53f3094569975ebf916964c3b36ddd7fa8fda03
Akhmad Herdian <akhmad.herdian@gmail.com>
2023-02-08 16:49:08 +0800

    Add basic index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Apexchart Laravel</title>
+</head>
+<body>
+    <div id="app" class="page">
+        <div class="flex-fill">
+            <div class="my-3 my-md-5">
+                <div class="container">
+                    <h1>Apexchart Laravel</h1>
+                    <div class="row justify-content-center">
+                        <div class="col-md-6">
+                            <div class="card">
+                                <div class="card-header">Jenis Chart</div>
+                                <div class="card-body">
+                                    Ini isi chart
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/apexcharts.js') }}"></script>
+</body>
+</html>
